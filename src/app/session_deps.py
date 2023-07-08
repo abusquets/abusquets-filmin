1495c425585e21a3d3a4604bda705da47f6671bf
Alexandre Busquets Triola <abusquets@gmail.com>
2023-07-08 16:49:39 +0200

    feat: Add refresh token

--- src/app/session_deps.py
+++ src/app/session_deps.py
@@ -1,35 +1,57 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from starlette.status import HTTP_403_FORBIDDEN
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
 
 from auth.utils import decode_token
 
 from app.schemas import Session
 
 
-async def get_current_session(
-    credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer()),
+async def _check_token(
+    token_type: str,
+    credentials: HTTPAuthorizationCredentials,
 ) -> Session:
     token = credentials.credentials
 
     if not token:
         raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN,
+            status_code=HTTP_401_UNAUTHORIZED,
             detail='Invalid authentication credentials.',
         )
 
     claims = decode_token(token)
 
     if claims is None:
-        raise HTTPException(status_code=403, detail='Invalid token or expired token.')
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail='Invalid token or expired token.')
+
+    if claims.get('type') != token_type:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail='Invalid token.')
 
     return Session(username=claims.get('sub'), profile=claims.get('profile'))
+
+
+async def check_access_token(
+    credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer()),
+) -> Session:
+    return await _check_token(
+        'a',
+        credentials,
+    )
+
+
+async def check_refresh_token(
+    credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer()),
+) -> Session:
+    return await _check_token(
+        'r',
+        credentials,
+    )
 
 
 async def is_admin_session(
     credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer()),
 ) -> Session:
-    session = await get_current_session(credentials)
+    session = await check_access_token(credentials)
 
     if not session.profile.is_admin:
         raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail='Not authorized.')
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import logging
 
-from typing import Optional, cast
+from typing import Literal, Optional, cast
 
 from jose import JWTError, jwt
 
@@ -11,26 +11,26 @@ from config import settings
 logguer = logging.getLogger(settings.APP_LOGGER_NAME)
 
 
-def _create_token(subject: str, claims: dict, expiration_minutes: int) -> str:
+def _create_token(type_token: Literal['a', 'r'], subject: str, claims: dict, expiration_minutes: int) -> str:
     now = datetime.utcnow()
     expires = datetime.utcnow() + timedelta(minutes=expiration_minutes)
-    payload = {'iss': 'calce', 'nbf': now, 'iat': now, 'exp': expires, 'sub': subject} | claims
+    payload = {'type': type_token, 'nbf': now, 'iat': now, 'exp': expires, 'sub': subject} | claims
 
     return cast(str, jwt.encode(payload | claims, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM))
 
 
 def create_access_token(subject: str, claims: dict) -> str:
-    return _create_token(subject, claims, settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return _create_token('a', subject, claims, settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
 
 def create_refresh_token(subject: str, claims: dict) -> str:
-    return _create_token(subject, claims, settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+    return _create_token('r', subject, claims, settings.REFRESH_TOKEN_EXPIRE_MINUTES)
 
 
 def decode_token(token: str) -> Optional[dict]:
     claims = None
     try:
-        claims = jwt.decode(token, settings.JWT_SECRET_KEY, issuer='calce', algorithms=[settings.JWT_ALGORITHM])
+        claims = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
     except JWTError:
-        logguer.debug('Ttoken verification failed!', extra={'token': token})
+        logguer.debug('Token verification failed!', extra={'token': token})
     return claims
